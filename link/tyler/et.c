eec209c129492256c409eceda73ba71c3830c136
waddlepon <tylerpoon@gmail.com>
2015-02-20 19:11:19 -0800

    Updated ET Sensor Test

---
+++ link/tyler/et.c
@@ -0,0 +1,19 @@
+#include "../constants.h"
+#include "../drive.h"
+#include "link_routines.h"
+
+void move_until_et(float threshold)
+{
+        motor(MOT_LEFT, SPD);
+        motor(MOT_RIGHT, SPD);
+        set_analog_pullup(ET, 0);
+        while(true)
+        {
+                if(analog10(ET) <= threshold)
+                {
+                        break;
+                }
+                msleep(50);
+        }
+        ao();
+}
