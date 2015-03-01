2226e2169a25e60b3f8f3ae9b78fd0fa3ba65cba
waddlepon <tylerpoon@gmail.com>
2015-02-28 19:58:09 -0800

    Changed Moving in square on line

--- link/link_routines.h
+++ link/link_routines.h
@@ -11,111 +11,113 @@
 
 void move_until_et(float threshold)
 {
-	motor(MOT_LEFT, 100);
-	motor(MOT_RIGHT, 100);
-	while(1)
-	{
-		//printf("ET: %d", analog_et(ET));
-		if(analog_et(ET) >= threshold)
-		{
-			break;
-		}
-		msleep(50);
-	}
-	ao();
+        motor(MOT_LEFT, 100);
+        motor(MOT_RIGHT, 100);
+        while(1)
+        {
+                //printf("ET: %d", analog_et(ET));
+                if(analog_et(ET) >= threshold)
+                {
+                        break;
+                }
+                msleep(50);
+        }
+        ao();
 }
 
 void servo_set(int port,int end,float time)//,float increment)
 {//position is from 0-2047
-	float increment = .01;
-	//printf("servo %d",port);
-	float curr,start = get_servo_position(port);
-	float i = ((end-start)/time)*increment;
-	curr = start;
-	if (start > end)
-	{
-		while(curr > end)
-		{
-			//printf("%f\n",curr);
-			set_servo_position(port,curr);
-			curr+=i;
-			msleep((long)(increment*1000));
-		}
-	}
-	else if (start < end)
-	{
-		while(curr < end)
-		{
-			//printf("%f\n",curr);
-			set_servo_position(port,curr);
-			curr+=i;
-			msleep((long)(increment*1000));
-		}
-	}
-	set_servo_position(port,end);
+        float increment = .01;
+        //printf("servo %d",port);
+        float curr,start = get_servo_position(port);
+        float i = ((end-start)/time)*increment;
+        curr = start;
+        if (start > end)
+        {
+                while(curr > end)
+                {
+                        //printf("%f\n",curr);
+                        set_servo_position(port,curr);
+                        curr+=i;
+                        msleep((long)(increment*1000));
+                }
+        }
+        else if (start < end)
+        {
+                while(curr < end)
+                {
+                        //printf("%f\n",curr);
+                        set_servo_position(port,curr);
+                        curr+=i;
+                        msleep((long)(increment*1000));
+                }
+        }
+        set_servo_position(port,end);
 }
 
 void lift_arm() 
 {
-	servo_set(ARM_SERVO, 2047, 5);
+        servo_set(ARM_SERVO, 2047, 5);
 }
 
 void ping()
 {
-	thread tid;
-	tid = thread_create(lift_arm);
-	thread_start(tid);
-	
-	// Replace with a square on the black line
-	// square_on_line();
-	
-	motor(MOT_LEFT, -20);
-	motor(MOT_RIGHT, -20);
-	msleep(3000);
-	forward(10)l
-	thread_destroy(tid);
+        thread tid;
+        tid = thread_create(lift_arm);
+        thread_start(tid);
+
+        // Replace with a square on the black line
+        // square_on_line();
+
+        motor(MOT_LEFT, -20);
+        motor(MOT_RIGHT, -20);
+        msleep(3000);
+        forward(10)l
+                thread_destroy(tid);
 }
 
 /**
  * Tunnel to the "cave"
  * PROP_SERVO must be enabled
-*/
+ */
 void e()
 {
-	ssp(PROP_SERVO, PROP_UP);
-	msleep(500);
-	//forward(10);
-	fd_with_time(MOT_LEFT, MOT_RIGHT, 1000);
-	ssp(PROP_SERVO, PROP_DOWN);
-	msleep(500);
-	fd_with_time(MOT_LEFT, MOT_RIGHT, 500);
-	ssp(PROP_SERVO, PROP_UP);
-	//forward(20);
-	msleep(500);
-	//forward(10);
-	fd_with_time(MOT_LEFT, MOT_RIGHT, 1000);
+        ssp(PROP_SERVO, PROP_UP);
+        msleep(500);
+        //forward(10);
+        fd_with_time(MOT_LEFT, MOT_RIGHT, 1000);
+        ssp(PROP_SERVO, PROP_DOWN);
+        msleep(500);
+        fd_with_time(MOT_LEFT, MOT_RIGHT, 500);
+        ssp(PROP_SERVO, PROP_UP);
+        //forward(20);
+        msleep(500);
+        //forward(10);
+        fd_with_time(MOT_LEFT, MOT_RIGHT, 1000);
 }
 
 void square_on_line()
 {
         while(analog(TOP_HAT_RIGHT) <= DARKNESS_THRESHOLD || analog(TOP_HAT_LEFT) <= DARKNESS_THRESHOLD) 
         {
-                ao();
                 if(analog(TOP_HAT_RIGHT) >= DARKNESS_THRESHOLD)
                 {
-                        motor(MOT_LEFT, 100);
+                        motor(MOT_LEFT, 300);
+                        motor(MOT_RIGHT,200);
                 }
                 else if(analog(TOP_HAT_LEFT) >= DARKNESS_THRESHOLD)
                 {
-                        motor(MOT_RIGHT, 100);
+                        motor(MOT_RIGHT, 300);
+                        motor(MOT_LEFT, 200);
                 }
                 else
                 {
-                        motor(MOT_RIGHT, 100);
-                        motor(MOT_LEFT, 100);
+                        motor(MOT_RIGHT, 300);
+                        motor(MOT_LEFT, 300);
                 }
                 msleep(100);
         }
+        ao();
 }
 
 #endif
